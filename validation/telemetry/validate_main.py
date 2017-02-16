daf998dc159115e32af1c4b3081956a4510e2277
Marco Castelluccio <mcastelluccio@mozilla.com>
2017-02-16 17:50:42 +0000

    Add sample pings for validation

--- validation/telemetry/validate_main.py
+++ validation/telemetry/validate_main.py
@@ -4,28 +4,36 @@ from jsonschema import validate, ValidationError
 LOCAL = os.path.dirname(__file__)
 SAMPLE_PING_PATH = os.path.join(LOCAL, 'sample_v4_ping.json')
 MAIN_SCHEMA_PATH = os.path.join(LOCAL, '../../telemetry/main.schema.json')
-
+MODULES_PING_PATHS = [os.path.join(LOCAL, 'modules_v1_ping_linux.json'), os.path.join(LOCAL, 'modules_v1_ping_windows.json')]
+MODULES_SCHEMA_PATH = os.path.join(LOCAL, '../../telemetry/modules.schema.json')
 
 class Test_validate(unittest.TestCase):
 
-    def setUp(self):
-        with open(SAMPLE_PING_PATH) as f:
-            self.ping = json.load(f)
+    def do_test_ping(self, schema_path, ping_path):
+        with open(schema_path) as f:
+            schema = json.load(f)
 
-    def test_main(self):
-        with open(MAIN_SCHEMA_PATH) as f:
-            main_schema = json.load(f)
+        with open(ping_path) as f:
+            ping = json.load(f)
+
         # The presence of $schema means that validate() will test
         # for specific JSON-Schema features
-        self.failUnless("$schema" in main_schema)
+        self.failUnless("$schema" in schema)
         # Sanity
-        self.failUnless("properties" in main_schema)
-        self.failUnless("required" in main_schema)
-        self.assertRaises(ValidationError, validate, {}, main_schema)
-        validate(self.ping, main_schema)
+        self.failUnless("properties" in schema)
+        self.failUnless("required" in schema)
+        self.assertRaises(ValidationError, validate, {}, schema)
+        validate(ping, schema)
 
-        self.ping['creationDate'] = self.ping['creationDate'][:1]
-        self.assertRaises(ValidationError, validate, self.ping, main_schema)
+        ping['creationDate'] = ping['creationDate'][:1]
+        self.assertRaises(ValidationError, validate, ping, schema)
+
+    def test_main(self):
+        self.do_test_ping(MAIN_SCHEMA_PATH, SAMPLE_PING_PATH)
+
+    def test_modules(self):
+        for path in MODULES_PING_PATHS:
+            self.do_test_ping(MODULES_SCHEMA_PATH, path)
 
 
 if __name__ == '__main__':
