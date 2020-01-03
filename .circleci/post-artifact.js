723350e308aae0b5e65d23d944ee89e5be68e4e6
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2020-01-03 15:27:30 -0800

    Add GH_AUTH_TOKEN

--- .circleci/post-artifact.js
+++ .circleci/post-artifact.js
@@ -2,7 +2,7 @@
  
 const bot = require("circle-github-bot").create();
  
-bot.comment(`
+bot.comment(process.env.GH_AUTH_TOKEN, `
 <h3>${bot.env.commitMessage}</h3>
 Demo: <strong>${bot.artifactLink('demo/index.html', 'demo')}</strong>
 `);
