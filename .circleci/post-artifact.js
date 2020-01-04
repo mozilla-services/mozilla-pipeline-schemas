a6d8aa1e1f1da423f91e0bbabd3a4a7c43c49a51
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2020-01-03 16:47:47 -0800

    Set home for proper artifact path

--- .circleci/post-artifact.js
+++ .circleci/post-artifact.js
@@ -27,8 +27,8 @@ if (diff_content) {
 
 bot.comment(process.env.GH_AUTH_TOKEN, `
 ### Integration report for "${bot.env.commitMessage}"
-Report for upstream: ${bot.artifactLink("app/test-report", upstream + ".report.json")}
-Report for latest commit: ${bot.artifactLink("app/test-report", head + ".report.json")}
+Report for upstream: ${bot.artifactLink(`${upstream}.report.json`, upstream)}
+Report for latest commit: ${bot.artifactLink(`${head}.report.json`, head)}
 
 ${content}
 `);
