6652f1fd532bb61163afb07144cb89ab7a8c3903
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2020-01-03 16:29:03 -0800

    Add content to the message

--- .circleci/post-artifact.js
+++ .circleci/post-artifact.js
@@ -3,10 +3,30 @@
 const bot = require("circle-github-bot").create();
 
 var fs = require("fs");
-var files = fs.readdirSync("/tmp");
+var files = fs.readdirSync("/tmp/test-reports");
 console.log(files);
 
+// example listing
+// 723350e.report.json
+// c88ebe5-723350e.diff
+// c88ebe5.report.json
+let diff_file = files.filter(x => x.endsWith(".diff"))[0];
+let diff_content = fs.readFileSync(diff_file, "utf8");
+let [upstream, head] = diff_file.split(".")[0].split("-");
+
+var content = "No changes detected.";
+if (diff_content) {
+    content = `#### ${diff_file}
+    \`\`\`diff
+    ${diff_content}
+    \`\`\`
+    `;
+}
+
 bot.comment(process.env.GH_AUTH_TOKEN, `
-<h3>${bot.env.commitMessage}</h3>
-${files}
+### Integration report for "${bot.env.commitMessage}"
+Report for upstream: ${bot.artifactLink("app/test-report", upstream + ".report.json")}
+Report for latest commit: ${bot.artifactLink("app/test-report", head + ".report.json")}
+
+${content}
 `);
