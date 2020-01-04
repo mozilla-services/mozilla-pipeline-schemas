2ca2ca329a21643c2ce1359344bb4db8fdef86af
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2020-01-03 16:02:48 -0800

    Add workspace and integration dependency

--- .circleci/post-artifact.js
+++ .circleci/post-artifact.js
@@ -1,8 +1,12 @@
 #!/usr/bin/env node
  
 const bot = require("circle-github-bot").create();
- 
+
+var fs = require("fs");
+var files = fs.readdirSync("/tmp");
+console.log(files);
+
 bot.comment(process.env.GH_AUTH_TOKEN, `
 <h3>${bot.env.commitMessage}</h3>
-Demo: <strong>${bot.artifactLink('demo/index.html', 'demo')}</strong>
+${files}
 `);
