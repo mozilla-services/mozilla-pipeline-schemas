15b5f64b20680a4060a35e6150824faeb404d5f3
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2020-01-06 12:51:19 -0800

    Add comments to post-artifact script

--- .circleci/post-artifact.js
+++ .circleci/post-artifact.js
@@ -1,28 +1,35 @@
 #!/usr/bin/env node
- 
+// A script for posting to Github from CircleCI. This requires GH_AUTH_TOKEN to
+// be set up, along-side CircleCI specific variables. See the source at [1] for
+// more details.
+// https://github.com/themadcreator/circle-github-bot/blob/master/src/index.ts
+
+const fs = require("fs");
 const bot = require("circle-github-bot").create();
 
-var fs = require("fs");
-
 let root = "/tmp/test-reports";
-var files = fs.readdirSync(root);
+let files = fs.readdirSync(root);
 console.log(files);
 
-// example listing
-// 723350e.report.json
-// c88ebe5-723350e.diff
-// c88ebe5.report.json
+// An example listing of files. The diff is created by comparing differences
+// between the upstream commit (mozilla-pipeline-schemas/master) and the report
+// generated from the PR
+//
+// ["723350e.report.json", "c88ebe5.report.json", "c88ebe5-723350e.diff"]
+
 let diff_file = files.filter(x => x.endsWith(".diff"))[0];
 let diff_content = fs.readFileSync(root + "/" + diff_file, "utf8");
 let [upstream, head] = diff_file.split(".")[0].split("-");
 
-var content = "No changes detected.";
-if (diff_content) {
-    content = `#### ${diff_file}
-    \`\`\`diff
-    ${diff_content}
-    \`\`\`
-    `;
+// Generate and post markdown
+var content = `#### ${diff_file}
+\`\`\`diff
+${diff_content}
+\`\`\`
+`;
+
+if (!diff_content) {
+    content =  "No changes detected.";
 }
 
 bot.comment(process.env.GH_AUTH_TOKEN, `
