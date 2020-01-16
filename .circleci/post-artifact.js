49b3161dd307e805bda44b7559eb4e9079cac4b1
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2020-01-16 12:27:03 -0800

    Bug 1607326 - Post integration artifacts as comment in PR (#469)
    * Add bot to post artifacts
    
    * Update executable mode
    
    * Add GH_AUTH_TOKEN
    
    * Add workspace and integration dependency
    
    * Add content to the message
    
    * Add directory root for reading contents of diff file
    
    * Set home for proper artifact path
    
    * Manually construct artifact url
    
    * Store artifacts from the same job that posts comments
    
    * Update workspace location
    
    * Add comments to post-artifact script
    
    * [testing] Intentially break a schema for report results
    
    * Put title in backticks
    
    * Revert "[testing] Intentially break a schema for report results"
    
    This reverts commit ecace426ca1a22ba2cd8ee7258c0497846797a19.
    
    * [testing] Break main schema for testing new ci credentials
    
    * Revert "[testing] Break main schema for testing new ci credentials"
    
    This reverts commit 9e2c9b50f259d4817b9ca7a39b45fac8d7dc8d4b.

---
+++ .circleci/post-artifact.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+// A script for posting to Github from CircleCI. This requires GH_AUTH_TOKEN to
+// be set up, along-side CircleCI specific variables. See the source at [1] for
+// more details.
+// https://github.com/themadcreator/circle-github-bot/blob/master/src/index.ts
+
+const fs = require("fs");
+const bot = require("circle-github-bot").create();
+
+let root = "/tmp/test-reports";
+let files = fs.readdirSync(root);
+console.log(files);
+
+// An example listing of files. The diff is created by comparing differences
+// between the upstream commit (mozilla-pipeline-schemas/master) and the report
+// generated from the PR
+//
+// ["723350e.report.json", "c88ebe5.report.json", "c88ebe5-723350e.diff"]
+
+let diff_file = files.filter(x => x.endsWith(".diff"))[0];
+let diff_content = fs.readFileSync(root + "/" + diff_file, "utf8");
+let [upstream, head] = diff_file.split(".")[0].split("-");
+
+// Generate and post markdown
+var content = `#### \`${diff_file}\`
+\`\`\`diff
+${diff_content}
+\`\`\`
+`;
+
+if (!diff_content) {
+    content =  "No changes detected.";
+}
+
+bot.comment(process.env.GH_AUTH_TOKEN, `
+### Integration report for "${bot.env.commitMessage}"
+[Report for upstream](${bot.env.buildUrl}/artifacts/0/app/test-reports/${upstream}.report.json)
+[Report for latest commit](${bot.env.buildUrl}/artifacts/0/app/test-reports/${head}.report.json)
+
+${content}
+`);
