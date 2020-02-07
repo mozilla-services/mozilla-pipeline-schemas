95275b7b83e55a72aea6b38ace9c888b7cc7e0f3
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2020-02-07 14:03:23 -0800

    Add parameterized tests with validation documents

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,21 +7,74 @@ http://doc.pytest.org/en/latest/example/parametrize.html
 
 import pytest
 from pathlib import Path
+import json
 
-schemas = Path(__file__).parent.parent / "schemas"
-validation = Path(__file__).parent.parent / "validation"
+ROOT = Path(__file__).parent.parent
+SCHEMAS_ROOT = ROOT / "schemas"
+VALIDATION_ROOT = ROOT / "validation"
 
 
+@pytest.fixture()
+def schemas():
+    schemas = {}
+    for path in SCHEMAS_ROOT.glob("**/*.schema.json"):
+        assert (
+            len(path.relative_to(ROOT).parts) <= 4
+        ), f"schemas directory too deep: {path}"
+        # Using the path parts allows proper handling of schemas that are not
+        # placed in the expected directory structure, like pioneer-study.
+        #
+        #   >>> schema.parts
+        #   ('validation', 'pocket', 'fire-tv-events.1.sample.pass.json')
+        #
+        # https://docs.python.org/3/library/pathlib.html#accessing-individual-parts
+        namespace = path.relative_to(ROOT).parts[1]
+        doctype, version = path.name.split(".")[:2]
+        qualifier = f"{namespace}.{doctype}.{version}"
+        with path.open() as fp:
+            schemas[qualifier] = json.load(fp)
+    return schemas
+
+
+# TODO: test the following cases
+# - example in wrong place
+# - example with the wrong name
+# - schema that doesn't exist
 def pytest_generate_tests(metafunc):
-    # TODO: iterate over validation documents
-    metafunc.parametrize("schema", "test")
-    for expect in ["pass", "fail"]:
-        if f"{expect}ing_document" not in metafunc.fixturenames:
-            continue
-        metafunc.parametrize(
-            f"{expect}ing_document",
-            [
-                f"telemetry.main.4.{expect}.json",
-                f"glean.glean.1.baseline.{expect}.json",
-            ],
-        )
+    """Build up a data-structure for parameterization of tests.
+
+    https://docs.pytest.org/en/2.8.7/parametrize.html#the-metafunc-object
+    """
+    examples = {"pass": {"params": [], "ids": []}, "fail": {"params": [], "ids": []}}
+
+    # find all of the examples
+    for path in sorted(VALIDATION_ROOT.glob("**/*.json")):
+        assert (
+            len(path.relative_to(ROOT).parts) == 3
+        ), f"validation directory too deep: {path}"
+        namespace = path.relative_to(ROOT).parts[1]
+        try:
+            doctype, version, _, expect, _ = path.name.split(".")
+        except ValueError:
+            raise ValueError(
+                "validation example name must match "
+                "'{doctype}.{version}.{validation_reason}.{pass|fail}.json', "
+                f"got: {path.name}"
+            )
+        assert expect in ("pass", "fail"), f"unknown example type: {path.name}"
+        qualifier = f"{namespace}.{doctype}.{version}"
+
+        with path.open() as fp:
+            example = json.load(fp)
+
+        examples[expect]["params"].append((qualifier, example))
+        examples[expect]["ids"].append(f"{namespace}/{path.name}")
+
+    # inject parameters into the relevant tests
+    for expect, examples in examples.items():
+        if f"{expect}ing_example" in metafunc.fixturenames:
+            metafunc.parametrize(
+                ["qualifier", f"{expect}ing_example"],
+                examples["params"],
+                ids=examples["ids"],
+            )
--- tests/test_validation.py
+++ tests/test_validation.py
@@ -1,6 +1,9 @@
-def test_validation_pass(schema, passing_document):
+import pytest
+
+
+def test_validation_pass(schemas, qualifier, passing_example):
     pass
 
 
-def test_validation_fail(schema, failing_document):
+def test_validation_fail(schemas, qualifier, failing_example):
     pass
