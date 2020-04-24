cfd06fee6d8e45653edfe941b927f4d436e1a9f1
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2020-04-24 12:26:19 -0700

    Post changes in generated BigQuery schema to comments (#525)
    * Add initial script for BigQuery CI
    
    * Generate bq schemas for head and base revisions
    
    * Refactor main routine into checkout_transpile_schema
    
    * Add diff between versions of bigquery schemas
    
    * Add test for checkout transpile schemas
    
    * Add tests for the diff
    
    * Remove hindsight and add jsonschema-transpiler
    
    * Add script to run a shell with current directory mounted
    
    * Run bigquery script and persist to ci workspace
    
    * Enable docker_caching_layer
    
    * Add an upstream branch to the ci checkout
    
    * Add options for specifying head and base ref
    
    * Checkout the reference instead of revision when restoring state
    
    * Use upstream/master as the base reference for ci
    
    * Use the correct app name when copying back to circleci
    
    * Specify relative path to folder with results
    
    * Add dependency to test for posting artifacts
    
    * Update post-artifact for BigQuery diff
    
    * Move section referring to head/base revisions into validation report
    
    * Update post-artifact to expand if diff is present
    
    * Use contextmanager for managing current git state
    
    * Add docstrings
    
    * Add tests for the bigquery script to ci
    
    * Use a tagged version of jsonschema-transpiler
    
    * Fix test due to lack of stashing in nested context
    
    * Update scripts/bigquery.py
    
    Co-Authored-By: Frank Bertsch <fbertsch@mozilla.com>
    
    * Update .circleci/post-artifact.js
    
    Co-Authored-By: Anna Scholtz <anna@scholtzan.net>
    
    * Rename bigquery to bigquery_schema_diff
    
    * Update README with instructions on runnning bigquery_schema_diff
    
    * Assert failure if untracked files in schema directories
    
    * Use find instead of filter in post-artifact
    
    Co-authored-by: Frank Bertsch <fbertsch@mozilla.com>
    Co-authored-by: Anna Scholtz <anna@scholtzan.net>

--- .circleci/post-artifact.js
+++ .circleci/post-artifact.js
@@ -7,35 +7,71 @@
 const fs = require("fs");
 const bot = require("circle-github-bot").create();
 
-let root = "/tmp/test-reports";
-let files = fs.readdirSync(root);
-console.log(files);
+function validation_report() {
+    let root = "/tmp/test-reports";
+    let files = fs.readdirSync(root);
+    console.log(files);
 
-// An example listing of files. The diff is created by comparing differences
-// between the upstream commit (mozilla-pipeline-schemas/master) and the report
-// generated from the PR
-//
-// ["723350e.report.json", "c88ebe5.report.json", "c88ebe5-723350e.diff"]
+    // An example listing of files. The diff is created by comparing differences
+    // between the upstream commit (mozilla-pipeline-schemas/master) and the report
+    // generated from the PR
+    //
+    // ["723350e.report.json", "c88ebe5.report.json", "c88ebe5-723350e.diff"]
 
-let diff_file = files.filter(x => x.endsWith(".diff"))[0];
-let diff_content = fs.readFileSync(root + "/" + diff_file, "utf8");
-let [upstream, head] = diff_file.split(".")[0].split("-");
+    let diff_file = files.find(x => x.endsWith(".diff"));
+    let diff_content = fs.readFileSync(root + "/" + diff_file, "utf8");
+    let [upstream, head] = diff_file.split(".")[0].split("-");
 
-// Generate and post markdown
-var content = `#### \`${diff_file}\`
+    var body = "No content detected.";
+    if (diff_content) {
+        body = `<details>
+<summary>Click to expand!</summary>
+
 \`\`\`diff
 ${diff_content}
 \`\`\`
+</details>
 `;
+    }
 
-if (!diff_content) {
-    content =  "No changes detected.";
+    // Generate and post markdown
+    let content = `
+[Report for upstream](${bot.env.buildUrl}/artifacts/0/app/test-reports/${upstream}.report.json)
+[Report for latest commit](${bot.env.buildUrl}/artifacts/0/app/test-reports/${head}.report.json)
+
+#### \`${diff_file}\`
+${body}
+`;
+    return content;
+}
+
+function bigquery_diff() {
+    let root = "/tmp/integration";
+    let files = fs.readdirSync(root);
+    console.log(files);
+    let diff_file = files.find(x => x.endsWith(".diff"));
+    let diff_content = fs.readFileSync(root + "/" + diff_file, "utf8");
+
+    var body = "No content detected."
+    if (diff_content) {
+        body = `<details>
+<summary>Click to expand!</summary>
+
+\`\`\`diff
+${diff_content}
+\`\`\`
+</details>
+`
+    }
+    var content = `#### \`${diff_file}\`
+${body}
+`;
+    return content;
 }
 
 bot.comment(process.env.GH_AUTH_TOKEN, `
 ### Integration report for "${bot.env.commitMessage}"
-[Report for upstream](${bot.env.buildUrl}/artifacts/0/app/test-reports/${upstream}.report.json)
-[Report for latest commit](${bot.env.buildUrl}/artifacts/0/app/test-reports/${head}.report.json)
+${validation_report()}
 
-${content}
+${bigquery_diff()}
 `);
