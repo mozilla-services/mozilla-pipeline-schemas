dcb0d802976f96d741e71e49b93eafddb8c5bd86
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2020-09-25 18:01:43 -0700

    Add command for generating compact set of columns

--- tests/test_mps_cli_bigquery.py
+++ tests/test_mps_cli_bigquery.py
@@ -1,13 +1,14 @@
 import os
+import json
 
 from click.testing import CliRunner
 
-from mozilla_pipeline_schemas.cli.bigquery import diff
+from mozilla_pipeline_schemas.cli.bigquery import diff, columns, transpile
 from utils import runif_cli_configured
 
 
 @runif_cli_configured
-def test_main(tmp_git):
+def test_bigquery_diff(tmp_git):
     # choose a base ref relative to HEAD, since the head ref may be master
     res = CliRunner().invoke(
         diff,
@@ -25,7 +26,7 @@ def test_main(tmp_git):
 
 
 @runif_cli_configured
-def test_main_duplicate(tmp_git):
+def test_bigquery_diff_duplicate(tmp_git):
     res = CliRunner().invoke(
         diff,
         [
@@ -44,3 +45,76 @@ def test_main_duplicate(tmp_git):
     assert (
         not next((tmp_git / "integration").glob("*.diff")).open().read()
     ), "diff should be empty"
+
+
+def test_bigquery_columns(tmp_path):
+    schema = [
+        {"name": "leaf", "mode": "NULLABLE", "type": "INT64"},
+        {"name": "repeated", "mode": "REPEATED", "type": "INT64"},
+        {
+            "name": "nested",
+            "mode": "NULLABLE",
+            "type": "RECORD",
+            "fields": [{"name": "leaf", "type": "INT64", "mode": "NULLABLE"}],
+        },
+        {
+            "name": "repeated_nested",
+            "mode": "REPEATED",
+            "type": "RECORD",
+            "fields": [{"name": "leaf", "type": "INT64", "mode": "NULLABLE"}],
+        },
+    ]
+    expected = sorted(
+        [
+            "root.leaf INT64",
+            "root.nested.leaf INT64",
+            "root.repeated.[] INT64",
+            "root.repeated_nested.[].leaf INT64",
+        ]
+    )
+    path = tmp_path / "test"
+    with path.open("w") as fp:
+        json.dump(schema, fp)
+    res = CliRunner().invoke(columns, [str(path)], catch_exceptions=False)
+    assert res.exit_code == 0
+    output = res.output.strip().split("\n")
+    assert output == expected
+
+
+@runif_cli_configured
+def test_bigquery_columns_from_transpiled(tmp_path):
+    schema = {
+        "type": "object",
+        "properties": {
+            "leaf": {"type": "integer"},
+            "nested": {"type": "object", "properties": {"leaf": {"type": "integer"}}},
+            "repeated": {"type": "array", "items": {"type": "integer"}},
+            "repeated_nested": {
+                "type": "array",
+                "items": {
+                    "type": "object",
+                    "properties": {"leaf": {"type": "integer"}},
+                },
+            },
+        },
+    }
+    expected = sorted(
+        [
+            "root.leaf INT64",
+            "root.nested.leaf INT64",
+            "root.repeated.[] INT64",
+            "root.repeated_nested.[].leaf INT64",
+        ]
+    )
+    path = tmp_path / "test"
+    with path.open("w") as fp:
+        json.dump(schema, fp)
+    res = CliRunner().invoke(transpile, [str(path)], catch_exceptions=False)
+    assert res.exit_code == 0
+
+    path = tmp_path / "transpiled"
+    with path.open("w") as fp:
+        fp.write(res.output)
+    res = CliRunner().invoke(columns, [str(path)], catch_exceptions=False)
+    output = res.output.strip().split("\n")
+    assert output == expected
