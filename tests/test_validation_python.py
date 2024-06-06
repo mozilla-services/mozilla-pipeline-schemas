a69d9903acb9a5420acc0f320b2d951aea9d9f0b
Ben Wu <12437227+BenWu@users.noreply.github.com>
2024-06-06 16:56:14 -0400

    [Bug 1898073] Change creationDate in telemetry.crash to date-time format (#816)

--- tests/test_validation_python.py
+++ tests/test_validation_python.py
@@ -1,11 +1,12 @@
 import pytest
 from jsonschema import validate
+from jsonschema._format import draft4_format_checker
 from jsonschema.exceptions import ValidationError
 
 
 def test_validation_pass_python(schemas, qualifier, passing_example):
     assert qualifier in schemas, f"{qualifier} missing from schemas"
-    validate(passing_example, schemas[qualifier])
+    validate(passing_example, schemas[qualifier], format_checker=draft4_format_checker)
     # TODO: raise all validation errors for debugging, using IValidator
     # interface. This requires knowing the JSON Schema spec ahead of time, for
     # example by ensuring $schema is set.
@@ -14,4 +15,4 @@ def test_validation_pass_python(schemas, qualifier, passing_example):
 def test_validation_fail_python(schemas, qualifier, failing_example):
     assert qualifier in schemas, f"{qualifier} missing from schemas"
     with pytest.raises(ValidationError):
-        validate(failing_example, schemas[qualifier])
+        validate(failing_example, schemas[qualifier], format_checker=draft4_format_checker)
